dbb791ec6724a0c821e85cfd8b5c651e950c2ac6
fullminji <alswl8184@gmail.com>
2024-01-02 15:53:00 +0900

    feat : 게임대기룸 페이지 생성

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import WaitingRoom from './pages/WaitingRoom';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/waiting-rooms" element={<WaitingRoom />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/WaitingRoom.tsx
@@ -0,0 +1,11 @@
+import '../style/sass/WaitingRoom.scss';
+
+const WaitingRoom = () => {
+  return (
+    <div className="waitingRoom">
+      <p>테스트</p>
+    </div>
+  );
+};
+
+export default WaitingRoom;
