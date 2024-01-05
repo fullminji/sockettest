d9f1335d44bbf0f9afd13c50a15166cdf4034e6c
홍지영 <hjy961021@gmail.com>
2024-01-05 15:59:12 +0900

    remove : 캐시 삭제

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import Router from './Router';
-import './style/sass/style.scss';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(<Router />);
